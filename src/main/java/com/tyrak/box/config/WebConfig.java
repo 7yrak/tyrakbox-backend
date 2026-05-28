aa4d724ebb00968cf1047d9a472ebe87dafe6986
Sergio Fica Troncoso <sergiofica@gmail.com>
2026-05-27 22:29:35 -0400

    version estable

--- src/main/java/com/tyrak/box/config/WebConfig.java
+++ src/main/java/com/tyrak/box/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://10.51.9.17", "http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(Arrays.asList("Content-Disposition"));
